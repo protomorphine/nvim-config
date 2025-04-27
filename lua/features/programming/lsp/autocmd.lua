1cfaec9367c0ed442189904d81907f429d7b2127
Daniil Zaytsev <kupernewfag@gmail.com>
2025-04-27 11:40:17 +0300

    autogen xml doc comments for charp lsp

--- lua/features/programming/lsp/autocmd.lua
+++ lua/features/programming/lsp/autocmd.lua
@@ -40,3 +40,31 @@ vim.api.nvim_create_autocmd("LspAttach", {
         require("virtualtypes").on_attach()
     end,
 })
+
+vim.api.nvim_create_autocmd('InsertCharPre', {
+    pattern = '*.cs',
+    callback = function()
+        local char = vim.v.char
+
+        if char ~= '/' then
+            return
+        end
+
+        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+        row, col = row - 1, col + 1
+        local bufnr = vim.api.nvim_get_current_buf()
+        local uri = vim.uri_from_bufnr(bufnr)
+
+        local params = {
+            _vs_textDocument = { uri = uri },
+            _vs_position = { line = row, character = col },
+            _vs_ch = char,
+            _vs_options = { tabSize = 4, insertSpaces = true },
+        }
+
+        -- NOTE: we should send textDocument/_vs_onAutoInsert request only after buffer has changed.
+        vim.defer_fn(function()
+            vim.lsp.buf_request(bufnr, 'textDocument/_vs_onAutoInsert', params)
+        end, 1)
+    end,
+})
--- lua/features/programming/lsp/servers/csharp.lua
+++ lua/features/programming/lsp/servers/csharp.lua
@@ -1,3 +1,48 @@
+---@class LineRange
+---@field line integer
+---@field character integer
+
+---@class EditRange
+---@field start LineRange
+---@field end LineRange
+
+---@class TextEdit
+---@field newText string
+---@field range EditRange
+
+---@param edit TextEdit
+local function apply_vs_text_edit(edit)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local start_line = edit.range.start.line
+  local start_char = edit.range.start.character
+  local end_line = edit.range['end'].line
+  local end_char = edit.range['end'].character
+
+  local newText = string.gsub(edit.newText, "\r", "")
+  local lines = vim.split(newText, "\n")
+
+  local placeholder_row = -1
+  local placeholder_col = -1
+
+  -- placeholder handling
+  for i, line in ipairs(lines) do
+    local pos = string.find(line, '%$0')
+    if pos then
+      lines[i] = string.gsub(line, '%$0', '', 1)
+      placeholder_row = start_line + i - 1
+      placeholder_col = pos - 1
+      break
+    end
+  end
+
+  vim.api.nvim_buf_set_text(bufnr, start_line, start_char, end_line, end_char, lines)
+
+  if placeholder_row ~= -1 and placeholder_col ~= -1 then
+    local win = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_cursor(win, { placeholder_row + 1, placeholder_col })
+  end
+end
+
 require("roslyn").setup({
     config = {
         capabilities = {
@@ -5,6 +50,35 @@ require("roslyn").setup({
                 full = true,
                 range = true,
             },
+            textDocument = {
+                _vs_onAutoInsert = { dynamicRegistration = false },
+            },
+        },
+        handlers = {
+            ["textDocument/hover"] = function(err, result, ctx, config)
+                if result and result.contents and result.contents.value then
+                    result.contents.value = result.contents.value:gsub("\\([^%w])", "%1")
+                end
+
+                vim.lsp.handlers["textDocument/hover"](err, result, ctx, config)
+
+                local parsed = require("pretty_hover.parser").parse(result)
+                vim.lsp.util.open_floating_preview(parsed.text, "markdown", {
+                    focus = true,
+                    focusable = true,
+                    wrap = true,
+                    wrap_at = 100,
+                    max_width = 100,
+                    border = "rounded",
+                    focus_id = "pretty-hover-example",
+                })
+            end,
+            ['textDocument/_vs_onAutoInsert'] = function(err, result, _)
+                if err or not result then
+                    return
+                end
+                apply_vs_text_edit(result._vs_textEdit)
+            end,
         },
         settings = {
             ["csharp|inlay_hints"] = {
