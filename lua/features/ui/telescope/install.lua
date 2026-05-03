69b6f88e4fe583cf91c45a33e0160a7558892cc3
Daniil Zaytsev <kupernewfag@gmail.com>
2026-05-03 14:47:19 +0300

    add telescope pluging for `vim.ui.select()`

--- lua/features/ui/telescope/install.lua
+++ lua/features/ui/telescope/install.lua
@@ -1,1 +1,4 @@
-return { "nvim-telescope/telescope.nvim", version = '*', dependencies = { "nvim-lua/plenary.nvim" }, }
+return {
+    { "nvim-telescope/telescope.nvim",           version = '*',                                     dependencies = { "nvim-lua/plenary.nvim", }, },
+    { "nvim-telescope/telescope-ui-select.nvim", dependencies = { "nvim-telescope/telescope.nvim" } }
+}
